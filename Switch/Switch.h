1467e1939353b5c31c23c1281542605651903500
Piers Mainwaring <piers@impossibly.org>
2016-01-03 10:14:04 -0700

    Initial Commit

---
+++ Switch/Switch.h
@@ -0,0 +1,19 @@
+//
+//  Switch.h
+//  Switch
+//
+//  Created by Piers Mainwaring on 1/3/16.
+//  Copyright © 2016 Playfair, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Switch.
+FOUNDATION_EXPORT double SwitchVersionNumber;
+
+//! Project version string for Switch.
+FOUNDATION_EXPORT const unsigned char SwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Switch/PublicHeader.h>
+
+
